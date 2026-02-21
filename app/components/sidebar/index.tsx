f029bfd76da55a22a083950dbc25416eb1be20c0
wealthymindkw <admin@wealthymindkw.com>
2026-02-21 22:40:21 +0300

    Update index.tsx

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -99,7 +99,7 @@ const Sidebar: FC<ISidebarProps> = ({
         </a>
 
         <a
-          href="حط_رابط_التلقرام_هني"
+          href="https://t.me/wealthvibrations"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center justify-center px-4 py-2 text-sm text-[#C5A059] bg-transparent border border-[#C5A059] rounded-md hover:bg-[#C5A05922] transition-colors font-serif text-center !border-opacity-50 !border-[0.5px]"
