d6a869dd9fea6954331e7994ddc34dc87ddbe7dd
wealthymindkw <admin@wealthymindkw.com>
2026-02-23 15:53:55 +0300

    Update index.tsx

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -349,7 +349,7 @@ const Main: FC<IMainProps> = () => {
               left: 0,
               right: 0,
               bottom: 0,
-              backgroundColor: 'rgba(0, 0, 0, 0.85))',
+              backgroundColor: 'rgba(0, 0, 0, 0.5))',
             }}
             onClick={hideSidebar}
           >
