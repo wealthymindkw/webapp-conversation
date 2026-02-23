6292326ac49dccd8355af2f81e30c666aed2b7e7
wealthymindkw <admin@wealthymindkw.com>
2026-02-23 19:13:11 +0300

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'ASK Hamad',
+  title: 'ASK Hamad AI',
   description: '',
   copyright: '',
   privacy_policy: '',
