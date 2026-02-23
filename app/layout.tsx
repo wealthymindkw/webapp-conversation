7d10fe38d37b386c35a3464242e58ba956777a18
wealthymindkw <admin@wealthymindkw.com>
2026-02-23 20:41:38 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import { getLocaleOnServer } from '@/i18n/server'
 import './styles/globals.css'
 import './styles/markdown.scss'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'ولثي مايند', // ← غيّر الاسم هنا
+  title: 'ولثي مايند',
   icons: {
     icon: 'https://assets.cdn.filesafe.space/rhWELETMkXWiHgXmcNv0/media/699a046320c035fe7bacda63.png',
     shortcut: 'https://assets.cdn.filesafe.space/rhWELETMkXWiHgXmcNv0/media/699a046320c035fe7bacda63.png',
@@ -19,15 +20,18 @@ const LocaleLayout = async ({
 }) => {
   const locale = await getLocaleOnServer()
   return (
-    <html lang={locale ?? 'en'} className="h-full">
-      <body className="h-full">
-        <div className="overflow-x-auto">
-          <div className="w-screen h-screen min-w-[300px]">
-            {children}
+    <ClerkProvider>
+      <html lang={locale ?? 'en'} className="h-full">
+        <body className="h-full">
+          <div className="overflow-x-auto">
+            <div className="w-screen h-screen min-w-[300px]">
+              {children}
+            </div>
           </div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
+
 export default LocaleLayout
